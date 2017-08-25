c2738d2334ffd1128d106c7f545db72aa002acf6
Braden.Scothern <BradenScothern@gmail.com>
2017-08-25 11:35:15 -0600

    changed default function implementation to open

--- GameThulhu/EventResponder/GamepadEventResponder.swift
+++ GameThulhu/EventResponder/GamepadEventResponder.swift
@@ -91,31 +91,31 @@ public protocol GamepadEventResponder {
 //MARK:- Default Implementation
 // These functions just pass the events up the UIResponder chain.
 extension UIResponder: GamepadEventResponder {
-    public func gamepadPausePressed(_ gamepad: Gamepad) {
+    open func gamepadPausePressed(_ gamepad: Gamepad) {
         next?.gamepadPausePressed(gamepad)
     }
     
-    public func gamepadButtonPressBegan(_ gamepad: Gamepad,_ button: Button) {
+    open func gamepadButtonPressBegan(_ gamepad: Gamepad,_ button: Button) {
         next?.gamepadButtonPressBegan(gamepad, button)
     }
     
-    public func gamepadButtonPressChanged(_ gamepad: Gamepad,_ button: Button) {
+    open func gamepadButtonPressChanged(_ gamepad: Gamepad,_ button: Button) {
         next?.gamepadButtonPressChanged(gamepad, button)
     }
     
-    public func gamepadButtonPressEnded(_ gamepad: Gamepad,_ button: Button) {
+    open func gamepadButtonPressEnded(_ gamepad: Gamepad,_ button: Button) {
         next?.gamepadButtonPressEnded(gamepad, button)
     }
     
-    public func gamepadDirectionalPadMovementBegan(_ gamepad: Gamepad, _ dPad: DirectionalPad) {
+    open func gamepadDirectionalPadMovementBegan(_ gamepad: Gamepad, _ dPad: DirectionalPad) {
         next?.gamepadDirectionalPadMovementBegan(gamepad, dPad)
     }
     
-    public func gamepadDirectionalPadChanged(_ gamepad: Gamepad, _ dPad: DirectionalPad) {
+    open func gamepadDirectionalPadChanged(_ gamepad: Gamepad, _ dPad: DirectionalPad) {
         next?.gamepadDirectionalPadChanged(gamepad, dPad)
     }
     
-    public func gamepadDirectionalPadMovementEnded(_ gamepad: Gamepad, _ dPad: DirectionalPad) {
+    open func gamepadDirectionalPadMovementEnded(_ gamepad: Gamepad, _ dPad: DirectionalPad) {
         next?.gamepadDirectionalPadMovementEnded(gamepad, dPad)
     }
 }
