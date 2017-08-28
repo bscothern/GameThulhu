ceca39b88fd74c2adf1aa178554a2b6e881208e0
Braden.Scothern <BradenScothern@gmail.com>
2017-08-28 14:04:51 -0600

    added support for  game controllers to know if they are connected to a device or not

--- GameThulhu/Gamepad/Gamepad.swift
+++ GameThulhu/Gamepad/Gamepad.swift
@@ -12,11 +12,39 @@ import GameController
 /// A wrapper class around `GCController` and its gamepad. Typically you should use `ExtendedController` or `MicroController`. This just provides consistency and a super type to both Controller types.
 @objc public class Gamepad: NSObject {
     ///MARK:- Properties
-    ///MARK: Internal
-    internal let controller: GCController
+    //MARK: Private
+    private var connectedObserver: GameControllerDiscovery.Observer! = nil
+    private var disconnectedObserver: GameControllerDiscovery.Observer! = nil
+    
+    //MARK: Public
+    
+    /// The `GCController` that backs up the `Gamepad`.
+    public let controller: GCController
+    
+    /// If `true` then the `controller` is still connected to the device and is valid to work with, if `false` it has been disconnected and will not work.
+    public private(set) var isConnected: Bool = true
 
     ///MARK:- Init
     public init?(controller: GCController) {
         self.controller = controller
+        
+        super.init()
+        
+        connectedObserver = GameControllerDiscovery.observeControllerConnected { [weak self](controller: GCController) in
+            if controller == self?.controller {
+                self?.isConnected = true
+            }
+        }
+        
+        disconnectedObserver = GameControllerDiscovery.observeControllerDisconnected { [weak self](controller: GCController) in
+            if controller == self?.controller {
+                self?.isConnected = false
+            }
+        }
+    }
+    
+    deinit {
+        GameControllerDiscovery.unobserveControllerConnected(observer: connectedObserver)
+        GameControllerDiscovery.unobserveControllerDisconnected(observer: disconnectedObserver)
     }
 }
